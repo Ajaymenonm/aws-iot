3bd7fda930c35d694c068c5abc08f951b48878ff
Ajay Menon <ajay.menon.m@gmail.com>
2022-03-13 21:14:13 -0600

    Add iot connection

--- core-device/app/main.js
+++ core-device/app/main.js
@@ -1,4 +1,22 @@
 const dhtsensor = require('node-dht-sensor').promises;
+const awsiot = require('aws-iot-device-sdk');
+
+
+const device = awsiot.device({
+    clientId: 'RPi-Core-Device',
+    host: 'a1suhadvbz0mhg-ats.iot.us-east-1.amazonaws.com',
+    port: 8883,
+    keyPath: './aws-auth/private.pem.key',
+    certPath: './aws-auth/certificate.pem.crt',
+    caPath: './aws-auth/AmazonRootCA1.cer',
+});
+
+device
+    .on('connect', () => {
+        console.log('Connecting to AWS  IoT Core');
+        // setInterval(() => getSensorData(sendData), 3000)
+        device.publish('deviceid/ondemanddata', JSON.stringify({ test_data: 1 }));
+    });
 
 
 const readSensorData = async () => {
