f9d3ec9dcd54e4f4d2884a79b25fe804565762aa
Ajay Menon <ajay.menon.m@gmail.com>
2022-03-13 18:32:45 -0600

    Add interval to read data

--- core-device/app/main.js
+++ core-device/app/main.js
@@ -4,10 +4,11 @@ const dhtsensor = require('node-dht-sensor').promises;
 const readSensorData = async () => {
     try {
         // 22 -> dht22 sensor; 4 -> gpio pin
-        const res = await dhtsensor.read(22, 4);
-        let temp = res.temperature.toFixed(1);
-        let humidity = res.humidity.toFixed(1);
-        console.log(`Temp: ${temp}, Humidity: ${humidity}`)
+        // const res = await dhtsensor.read(22, 4);
+        // let temp = res.temperature.toFixed(1);
+        // let humidity = res.humidity.toFixed(1);
+        // console.log(`Temp: ${temp}, Humidity: ${humidity}`)
+        console.log(`Temp: 10, Humidity: 20`)
     } catch (err) {
         console.error(`error reading sensor data: ${err}`)
     }
@@ -15,7 +16,11 @@ const readSensorData = async () => {
 
 async function main() {
     console.log(`********************Starting Core Device App********************`)
-    readSensorData()
+
+    // random upload time to avoid bandwidth spike
+    // const interval = Math.floor(Math.random() * (5 - 1) + 1)
+    setInterval(() => readSensorData(), 2000)
+
 }
 
 main().catch(console.error)
