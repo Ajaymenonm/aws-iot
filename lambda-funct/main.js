73da32778ec10d83076b10cdb55401f07d838e67
Ajay Menon <ajay.menon.m@gmail.com>
2022-03-15 23:32:57 -0600

    Make changes to upload to aws lambda

--- lambda-funct/main.js
+++ lambda-funct/main.js
@@ -5,16 +5,17 @@ const S3 = new AWS.S3();
 const unzipAsync = util.promisify(zlib.brotliDecompress);
 
 
-// exports.handler = async (event) => {
-const main = async (event) => {
+exports.handler = async (event) => {
+    // const main = async (event) => {
     // console.log('Received event:', JSON.stringify(event, null, 2));
     // const payload = JSON.stringify(event, null, 2)
-
-    const payload = {
-        "compresseddata": "G9MAQJwHxi3zLjQImv5OiIVagWyvD4jbUl9+uOc3dEtX0CAI3mCx6JQIjE5ODgzfDvxTlGBGqUcmeYzlDAYGDqKVJzkFOq0mccB8e/WVn1bk2IelMMfIS6w98fFV3TWCyaLaygA=",
-        "file": "2022_03_16_04_40_50.csv",
-        "deviceId": "device1234"
-    }
+    let payload = event;
+    console.log('Received event:', event);
+    // const payload = {
+    //     "compresseddata": "G9MAQJwHxi3zLjQImv5OiIVagWyvD4jbUl9+uOc3dEtX0CAI3mCx6JQIjE5ODgzfDvxTlGBGqUcmeYzlDAYGDqKVJzkFOq0mccB8e/WVn1bk2IelMMfIS6w98fFV3TWCyaLaygA=",
+    //     "file": "2022_03_16_04_40_50.csv",
+    //     "deviceId": "device1234"
+    // }
 
     const encodedData = payload.compresseddata
     const decompressedtelemetry = await decompressMessage(encodedData);
@@ -48,7 +49,7 @@ const decompressMessage = async (encodedData) => {
 const uploadToS3 = async (deviceid, filename, decompressedtelemetry) => {
     try {
         const params = {
-            Bucket: '',
+            Bucket: 'all-device-telemetry',
             Key: `${deviceid}/${filename}`,
             Body: JSON.stringify(decompressedtelemetry),
             ContentType: 'application/json; charset=utf-8'
@@ -60,4 +61,4 @@ const uploadToS3 = async (deviceid, filename, decompressedtelemetry) => {
     }
 }
 
-main()
+// main()
