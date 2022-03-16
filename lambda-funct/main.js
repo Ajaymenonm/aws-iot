8876ab6300b7243d81960df74caaf2148b45619f
Ajay Menon <ajay.menon.m@gmail.com>
2022-03-15 23:10:44 -0600

    Add decompress and s3 put funct

--- lambda-funct/main.js
+++ lambda-funct/main.js
@@ -1,5 +1,63 @@
-// Handler lamda function
-exports.handler = async (event) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
+const zlib = require('zlib');
+const util = require('util');
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3();
+const unzipAsync = util.promisify(zlib.brotliDecompress);
+
+
+// exports.handler = async (event) => {
+const main = async (event) => {
+    // console.log('Received event:', JSON.stringify(event, null, 2));
+    // const payload = JSON.stringify(event, null, 2)
+
+    const payload = {
+        "compresseddata": "G9MAQJwHxi3zLjQImv5OiIVagWyvD4jbUl9+uOc3dEtX0CAI3mCx6JQIjE5ODgzfDvxTlGBGqUcmeYzlDAYGDqKVJzkFOq0mccB8e/WVn1bk2IelMMfIS6w98fFV3TWCyaLaygA=",
+        "file": "2022_03_16_04_40_50.csv",
+        "deviceId": "device1234"
+    }
+
+    const encodedData = payload.compresseddata
+    const decompressedtelemetry = await decompressMessage(encodedData);
+
+    if (Object.keys(decompressedtelemetry).length > 0) {
+        const filename = payload.file
+        const deviceid = payload.deviceid
+        await uploadToS3(deviceid, filename, decompressedtelemetry)
+    }
+
     return true
 }
+
+
+const decompressMessage = async (encodedData) => {
+    try {
+        if (encodedData.length > 0) {
+            const compresseddata = Buffer.from(encodedData, 'base64');
+            const decompressedmessageraw = await unzipAsync(compresseddata);
+            decompressedmessage = decompressedmessageraw.toString('utf8');
+            console.log(`decompressed message: ${decompressedmessage}`);
+            console.log(`decompression successful`);
+            return decompressedmessagern
+        }
+    } catch (err) {
+        console.error(`error decompressing message: ${err}`);
+    }
+}
+
+
+const uploadToS3 = async (deviceid, filename, decompressedtelemetry) => {
+    try {
+        const params = {
+            Bucket: '',
+            Key: `${deviceid}/${filename}`,
+            Body: JSON.stringify(decompressedtelemetry),
+            ContentType: 'application/json; charset=utf-8'
+        }
+        await S3.putObject(params).promise();
+        console.log("Upload Completed");
+    } catch (err) {
+        console.log(`error uploading to s3 ${err}`);
+    }
+}
+
+main()
